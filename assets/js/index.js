4a5d63c716b4ada2766dfd12d2efdbd8e81a7bf9
Pietro Ardizzone <95643859+pio032@users.noreply.github.com>
2025-05-19 09:49:31 +0200

    add home

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+const ct = document.getElementById('ct');
+ct.addEventListener("click", function(){
+    window.location.href="/country.html"
+})
+const pl = document.getElementById('pl')
+pl.addEventListener("click", function(){
+    window.location.href="/population.html"
+})
