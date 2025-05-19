66d7598735b480c576d4f64e57e7e0f5279cc7f3
Pietro Ardizzone <95643859+pio032@users.noreply.github.com>
2025-05-19 17:07:20 +0200

    fix gen country

--- assets/js/country.js
+++ assets/js/country.js
@@ -1,11 +1,23 @@
-document.getElementById('errore').style.display='none'
+
 //---------------------------------------------------------CODICE-PAESI-RANDOM-------------------------------------------------------------------
 function paese() {
-    const v = Math.floor(Math.random() * 249);
-    const countryCodes = [
-        4, 8, 10, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 50, 51, 52, 56, 60, 64, 68, 70, 72, 74, 76, 84, 86, 96, 100, 104, 108, 112, 116, 120, 124, 126, 132, 136, 140, 144, 148, 152, 156, 162, 166, 170, 174, 175, 178, 180, 184, 188, 191, 192, 196, 203, 204, 208, 212, 214, 218, 222, 226, 231, 232, 233, 234, 238, 239, 242, 246, 250, 254, 258, 260, 262, 266, 270, 275, 276, 288, 292, 296, 300, 304, 308, 312, 316, 320, 324, 328, 332, 334, 336, 340, 344, 348, 352, 356, 360, 364, 368, 372, 376, 380, 388, 392, 398, 400, 404, 408, 410, 414, 417, 418, 422, 426, 428, 430, 434, 438, 440, 442, 446, 450, 454, 458, 462, 466, 470, 474, 478, 480, 484, 492, 496, 498, 499, 500, 504, 508, 512, 516, 520, 524, 528, 540, 554, 558, 562, 566, 570, 574, 580, 578, 580, 583, 584, 585, 586, 591, 598, 600, 604, 608, 612, 616, 620, 624, 630, 634, 638, 642, 643, 646, 652, 654, 659, 662, 663, 666, 670, 674, 678, 682, 686, 688, 690, 694, 702, 703, 704, 705, 706, 710, 724, 732, 736, 740, 744, 748, 752, 756, 760, 762, 764, 768, 772, 776, 780, 784, 788, 792, 795, 796, 800, 804, 807, 818, 826, 831, 832, 833, 834, 835, 836, 840, 850, 854, 858, 860, 862, 876, 882, 887, 888, 891, 894, 896, 900, 901, 902, 903, 904, 905, 906, 907, 908, 910, 911, 912, 913, 914, 915, 916, 917, 918, 919, 920, 921, 922, 923, 924, 925, 926, 927, 928, 929, 930, 931, 932, 933, 934, 935, 936, 937, 938, 939, 940, 941, 942, 943, 944, 945, 946, 947, 948, 949, 950, 951, 952, 953, 954, 955, 956, 957, 958, 959, 960, 961, 962, 963, 964, 965, 966, 967, 968, 969, 970, 971, 972, 973, 974, 975, 976, 977, 978, 979, 980, 981, 982, 983, 984, 985, 986, 987, 988, 989, 990, 991, 992, 993, 994, 995, 996, 997, 998, 999
+    
+    const c =Math.floor(Math.random() * 4);
+    const countryCodes1 = [
+        4, 8, 10, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 50, 51, 756, 760, 762, 929, 930, 931, 932, 933, 934, 935, 936, 937, 938, 939, 940, 941, 942, 943, 944, 945, 946, 947, 948, 949, 950, 951, 952, 953, 954, 955, 956, 957, 958, 959, 960, 961, 962, 963, 964, 965, 966, 967, 968, 969, 970, 971, 972, 973, 974, 975, 976, 977, 978, 979, 980, 981, 982, 983, 984, 985, 986, 987, 988, 989, 990, 991, 992, 993, 994, 995, 996, 997, 998, 999
     ];
-    return countryCodes[v];
+    const countryCodes2 = [64, 68, 70, 72, 74, 76, 84, 86, 96, 100, 104, 108, 112, 116, 120, 124, 126, 132, 136, 140, 144, 148, 152, 156, 162, 166, 170, 174, 175, 178, 180, 184, 188, 191, 192, 196, 203, 204, 208, 212, 214, 218, 222, 226, 231, 232, 233, 234, 238, 239, 242, 246, 250, 254, 258, 260, 262, 266, 270, 275, 276, 288, 292, 296, 300, 304, 308, 312, 316, 320, 324, 328, 332, 334, 336, 340, 344, 348, 352, 356, 360, 364, 368, 372, 376, 380, 388, 392, 398, 400, 404, 408, 410, 414, 417, 418, 422, 426, 428, 430, 434, 438, 440, 442, 446, 450, 454, 458, 462, 466, 470, 474, 478, 480, 484, 492, 496];
+    const countryCodes3= [ 52, 56, 60, 498, 499, 500, 504, 508, 512, 516, 520, 524, 528, 540, 554, 558, 562, 566, 570, 574, 580, 578, 580, 583, 584, 585, 586, 591, 598, 600, 604, 608, 612, 616, 620, 624, 630, 634, 638, 642, 643, 646, 652, 654, 659, 662, 663, 666, 670, 674, 678, 682, 686, 688, 690, 694, 702, 703, 704, 705, 706, 710, 724, 732, 736, 740, 744, 748, 752];
+    const countryCodes4=[764, 768, 772, 776, 780, 784, 788, 792, 795, 796, 800, 804, 807, 818, 826, 831, 832, 833, 834, 835, 836, 840, 850, 854, 858, 860, 862, 876, 882, 887, 888, 891, 894, 896, 900, 901, 902, 903, 904, 905, 906, 907, 908, 910, 911, 912, 913, 914, 915, 916, 917, 918, 919, 920, 921, 922, 923, 924, 925, 926, 927, 928] ;
+    const everCountry = [
+        countryCodes1,
+        countryCodes2,
+        countryCodes3,
+        countryCodes4
+    ];
+    const v = Math.floor(Math.random() * everCountry[c].length)+1;
+    console.log("c: "+c+" v: "+v+" numero: "+everCountry[c][v])
+    return everCountry[c][v];
 }
 //---------------------------------------------------------GENERAZIONE-INIZIALE------------------------------------------------------------------
 async function country() {
@@ -24,6 +36,7 @@ async function country() {
     }
 }
 
+//--------------------------------------------------DEFAULT---------------------------------------------------------------------------------
 (async () => {
      sx = await country();
      dx = await country();
@@ -35,9 +48,198 @@ async function country() {
     let bbox = `${lon-delta},${lat-delta},${lon+delta},${lat+delta}`;
     document.getElementById('imgSx').src =
     `https://www.openstreetmap.org/export/embed.html?bbox=${bbox}&layer=mapnik&marker=${lat},${lon}`;
-    document.getElementById('imgDx').src = "https://countryflagsapi.netlify.app/flag/" + dx[0].cca2 + ".svg";
+    let latD = dx[0].latlng[0];
+    let lonD = dx[0].latlng[1];
+    let deltaD = 12; 
+    let bboxD = `${lonD-deltaD},${latD-deltaD},${lonD+deltaD},${latD+deltaD}`;
+    document.getElementById('imgDx').src =
+    `https://www.openstreetmap.org/export/embed.html?bbox=${bboxD}&layer=mapnik&marker=${latD},${lonD}`;
     document.getElementById('titleSx').innerText =sx[0].name.common;
     document.getElementById('titleDx').innerText =dx[0].name.common;
 
 
 })();
+
+//---------------------------------------------------------GENERA---------------------------------------------------------------------------
+async function generaSX(){
+     sx = await country();
+    let lat = sx[0].latlng[0];
+    let lon = sx[0].latlng[1];
+    let delta = 4; 
+    let bbox = `${lon-delta},${lat-delta},${lon+delta},${lat+delta}`;
+    document.getElementById('imgSx').src =
+    `https://www.openstreetmap.org/export/embed.html?bbox=${bbox}&layer=mapnik&marker=${lat},${lon}`;
+    document.getElementById('titleSx').innerText =sx[0].name.common;
+    
+}
+async function generaDX(){
+    dx = await country();
+    let latD = dx[0].latlng[0];
+    let lonD = dx[0].latlng[1];
+    let deltaD = 12; 
+    let bboxD = `${lonD-deltaD},${latD-deltaD},${lonD+deltaD},${latD+deltaD}`;
+    document.getElementById('imgDx').src =
+    `https://www.openstreetmap.org/export/embed.html?bbox=${bboxD}&layer=mapnik&marker=${latD},${lonD}`;
+    document.getElementById('titleDx').innerText =dx[0].name.common;
+}
+
+
+function verifica(){
+    if(sx[0].area>dx[0].area){
+      return true;
+    }else{
+      return false;
+    }
+}
+
+
+
+//----------------------------------------------------GLOBAL-------------------------------------------------------------------------------
+ const win = localStorage.getItem('winC');
+ let score = 0;
+ let wSx = 0;
+ let wDx = 0;
+//----------------------------------------------------INIZIALIZZAZIONE---------------------------------------------------------------------
+    document.getElementById('errore').style.display='none'
+    document.getElementById('score').innerText=score;
+    document.getElementById('sx').classList.add('bordo-default');
+    document.getElementById('dx').classList.add('bordo-default')
+    document.getElementById('risp').innerText="...";
+//---------------------------------------------------CLICK-DX-SX-----------------------------------------------------------------------------
+
+document.getElementById('dx').addEventListener("click", function(){
+     document.getElementById('dx').style.pointerEvents = 'none';
+     document.getElementById('risp').innerText="";
+    if(wDx==0){
+    animaNumero(document.getElementById('valDx'), dx[0].area);
+    }
+    if(wSx==0){
+     animaNumero(document.getElementById('valSx'), sx[0].area);
+    }
+   
+    
+    if(!verifica()){
+       
+        wDx++;
+       score++;
+       document.getElementById('feedback').classList.add('success')
+       document.getElementById('risp').classList.add('fas', 'fa-check-circle');
+       document.getElementById('dx').classList.add('bordo-verde')
+       document.getElementById('sx').classList.add('bordo-rosso');
+       document.getElementById('score').innerText=score;
+       if(wDx>1){
+       
+       setTimeout(generaDX, 2000);
+       setTimeout(init, 2000, false);
+       wDx=0;
+       }
+       wSx=0;
+       setTimeout(generaSX, 2000);
+       setTimeout(init, 2000, true);
+       
+
+    }else{
+        document.getElementById('feedback').classList.add('error')
+        document.getElementById('risp').classList.add('fas', 'fa-times-circle');
+        document.getElementById('sx').classList.add('bordo-verde')
+        document.getElementById('dx').classList.add('bordo-rosso');
+        perdita();
+    }
+})
+
+   document.getElementById('sx').addEventListener("click", function(){
+    document.getElementById('sx').style.pointerEvents = 'none';
+    document.getElementById('risp').innerText="";
+   if(wSx==0){
+    animaNumero(document.getElementById('valSx'), sx[0].area);
+   }
+   if(wDx==0){
+    animaNumero(document.getElementById('valDx'), dx[0].area);
+    }
+
+    if(verifica()){
+        
+        wSx++;
+        score++;
+        document.getElementById('feedback').classList.add('success')
+        document.getElementById('risp').classList.add('fas', 'fa-check-circle');
+        document.getElementById('sx').classList.add('bordo-verde')
+        document.getElementById('dx').classList.add('bordo-rosso');
+        document.getElementById('score').innerText=score;
+        if(wSx>1){
+       
+        setTimeout(generaSX, 2000);
+        setTimeout(init, 2000, true);
+        wSx=0;
+        }
+        wDx=0;
+        setTimeout(generaDX, 2000);
+        setTimeout(init, 2000, false);
+       
+    }else{
+        document.getElementById('feedback').classList.add('error')
+        document.getElementById('risp').classList.add('fas', 'fa-times-circle');
+        document.getElementById('sx').classList.add('bordo-rosso');
+        document.getElementById('dx').classList.add('bordo-verde')
+         perdita();
+    }
+    
+})
+
+//----------------------------------------------------------ANIMAZIONE-----------------------------------------------------------------
+function animaNumero(element, target, durata = 1000) {
+    const end = Number(target);
+    const start = end/2;
+    const startTime = performance.now();
+
+    function update(currentTime) {
+        const elapsed = currentTime - startTime;
+        const progress = Math.min(elapsed / durata, 1);
+        const value = Math.floor(progress * (end - start) + start);
+        element.innerText = value.toLocaleString('it-IT');
+        if (progress < 1) {
+            requestAnimationFrame(update);
+        } else {
+            element.innerText = end.toLocaleString('it-IT');
+        }
+    }
+    requestAnimationFrame(update);
+}
+
+//-----------------------------------------INIT--------------------------------------------------------------------------------
+function init(dx){
+    document.getElementById('dx').style.pointerEvents = 'auto';
+    document.getElementById('sx').style.pointerEvents = 'auto';
+     document.getElementById('sx').classList.remove('bordo-verde', 'bordo-rosso', 'bordo-default');
+    document.getElementById('dx').classList.remove('bordo-verde', 'bordo-rosso', 'bordo-default');
+    document.getElementById('sx').classList.add('bordo-default');
+    document.getElementById('dx').classList.add('bordo-default')
+    document.getElementById('feedback').classList.remove('success', 'error');
+    document.getElementById('risp').className = '';
+    document.getElementById('risp').innerText = "...";
+    if(dx){
+    document.getElementById('valSx').innerText = "";
+    }else{
+    document.getElementById('valDx').innerText = "";
+    } 
+}
+
+//----------------------------------------PERSO--------------------------------------------------------------------------------
+
+function perdita(){
+     document.getElementById('errore').style.display='block'
+     document.getElementById('attuale').innerText="Hai totalizzato: "+score
+     if(win && win>score){
+        document.getElementById('local').innerText="Il tuo record è: "+win
+     }else{
+        localStorage.setItem('winC', score)
+        document.getElementById('local').innerText="Il tuo record è: "+ localStorage.getItem('winC')
+     }
+}
+
+
+
+//------------------------------------TORNA-ALLA-HOME--------------------------------------------------------------------------
+document.getElementById('home').addEventListener("click", function(){
+    window.location.href = "/index.html"
+})
